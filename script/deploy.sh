f6a20507a0b3e7f0b276243eecbd1047a8b0d07f
jellycsc <chenjie.ni@mail.utoronto.ca>
2019-01-02 22:20:16 +0800

    Set up travis-ci

---
+++ script/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+root_dir=`pwd`
+
+for filename in *; do
+	cd $root_dir
+    if [ -d "$filename" ]; then
+    	cd "$filename"
+        rsync -rqz --delete-after --rsync-path="mkdir -p ${DEPLOY_DIRECTORY}/${filename} && rsync" _book/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY/${filename}
+    fi
+done
